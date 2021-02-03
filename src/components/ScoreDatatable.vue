8295fbeb76e8b442486c0cd9e329804f57e65228
mdieudonne <michael.dieudonne@outlook.com>
2021-02-03 17:16:47 +0100

    fix: typo

--- src/components/ScoreDatatable.vue
+++ src/components/ScoreDatatable.vue
@@ -13,7 +13,7 @@
       <v-toolbar
           flat
       >
-        <v-toolbar-title>Gestion des note de {{ student.lastname }} {{ student.firstname }}</v-toolbar-title>
+        <v-toolbar-title>Gestion des notes de {{ student.lastname }} {{ student.firstname }}</v-toolbar-title>
         <v-divider
             class="mx-4"
             inset
